45ae5180bdcd01b8969cff19ea4eea311b4ac0c3
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-11-04 14:28:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.find {|word| word.length > 4}
+  words.find {|word| word.length > 4} ? true : false
 end
 
 def find_the_cheese# code an argument here
